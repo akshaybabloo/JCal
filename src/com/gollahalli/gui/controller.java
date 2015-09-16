c50e18a3e5b82b69e285e288be6b4bb7a581a189
Akshay Raj Gollahalli <akshaybabloo@gmail.com>
2015-09-16 19:57:49 +1200

    tableview added for all

--- src/com/gollahalli/gui/controller.java
+++ src/com/gollahalli/gui/controller.java
@@ -58,18 +58,12 @@ public class Controller {
 
     public static final Logger logger = LoggerFactory.getLogger(Controller.class);
 
-    public void initialize(){
+    public void initialize() {
         logger.info("controller started");
 
-        data = FXCollections.observableArrayList(
-//                new PaymentsTable("Jacob", "Smith", "jacob.smith@example.com", "Smith"),
-//                new PaymentsTable("Isabella", "Johnson", "isabella.johnson@example.com", "Smith"),
-//                new PaymentsTable("Ethan", "Williams", "ethan.williams@example.com", "Smith"),
-//                new PaymentsTable("Emma", "Jones", "emma.jones@example.com", "Smith"),
-//                new PaymentsTable("Michael", "Brown", "michael.brown@example.com", "Smith")
 
-//                new PaymentsTable("Michael", "Brown", "michael.brown@example.com", "Smith")
-        );
+        data = FXCollections.observableArrayList();
+
 
         yearColumn.setCellValueFactory(new PropertyValueFactory<PaymentsTable, String>("Year"));
         interestColumn.setCellValueFactory(new PropertyValueFactory<PaymentsTable, String>("Interest"));
@@ -77,6 +71,7 @@ public class Controller {
         balanceColumn.setCellValueFactory(new PropertyValueFactory<PaymentsTable, String>("Balance"));
 
         paymentsAnnual.setItems(data);
+        
 
         repayment_type.getItems().addAll("Monthly", "Bi-Monthly", "Fortnightly", "Yearly", "Quarterly", "Weekly", "Daily");
 
@@ -90,7 +85,8 @@ public class Controller {
             String years_text_string = years.getText();
             String interest_text_string = interest.getText();
 
-            if (loan_amount_string.isEmpty() || weeks_text_string.isEmpty() || years_text_string.isEmpty() || interest_text_string.isEmpty()){
+            if (loan_amount_string.isEmpty() || weeks_text_string.isEmpty() || years_text_string.isEmpty() || interest_text_string.isEmpty()) {
+                logger.error("values were missing.");
                 Alert alert = new Alert(Alert.AlertType.ERROR);
                 alert.setTitle("Error Dialog");
                 alert.setHeaderText("There seems to be an error!");
@@ -105,69 +101,125 @@ public class Controller {
 
             Text final_text = new Text();
 
-            switch (repayment_type_text){
+            switch (repayment_type_text) {
                 case "Monthly":
                     logger.info("Monthly payments selected");
-                    double[] monthly_output = calculate.compound_interest(loan_amount_text,interest_text,years_text,12.0);
+                    double[][] monthly_output = calculate.compound_interest(loan_amount_text, interest_text, years_text, 12.0);
                     final_text.setText("For the loan amount of " + loan_amount_text + ", at time interval of " + years_text +
-                            " years and " + weeks_text + " weeks at an interest rate of " + interest_text + " % the interest you would have to pay is $" + monthly_output[1] +
-                            ". The total amount you wold have to pay would be $" + monthly_output[0]);
+                            " years and " + weeks_text + " weeks at an interest rate of " + interest_text + " % the interest you would have to pay is $" + monthly_output[1][monthly_output[1].length - 1] +
+                            ". The total amount you wold have to pay would be $" + monthly_output[0][monthly_output[0].length - 1]);
                     summary.getChildren().add(final_text);
+
+                    for (int i = 1; i < monthly_output[0].length; i++) {
+                        PaymentsTable paymentsTable = new PaymentsTable();
+                        paymentsTable.year.setValue(monthly_output[0][i]);
+                        paymentsTable.principal.setValue(monthly_output[1][i]);
+                        data.add(paymentsTable);
+                    }
                     break;
 
                 case "Yearly":
                     logger.info("Yearly payments selected");
-                    double[] yearly_output = calculate.compound_interest(loan_amount_text,interest_text,years_text,1.0);
+                    paymentsAnnual.getItems().clear();
+                    double[][] yearly_output = calculate.compound_interest(loan_amount_text, interest_text, years_text, 1.0);
                     final_text.setText("For the loan amount of " + loan_amount_text + ", at time interval of " + years_text +
-                            " years and " + weeks_text + " weeks at an interest rate of " + interest_text + " % the interest you would have to pay is $" + yearly_output[1] +
-                            ". The total amount you wold have to pay would be $" + yearly_output[0]);
+                            " years and " + weeks_text + " weeks at an interest rate of " + interest_text + " % the interest you would have to pay is $" + yearly_output[0][yearly_output[0].length - 1] +
+                            ". The total amount you wold have to pay would be $" + yearly_output[0][yearly_output[0].length - 1]);
                     summary.getChildren().add(final_text);
+
+                    for (int i = 1; i < yearly_output[0].length; i++) {
+                        PaymentsTable paymentsTable = new PaymentsTable();
+                        paymentsTable.year.setValue(yearly_output[0][i]);
+                        paymentsTable.principal.setValue(yearly_output[1][i]);
+                        data.add(paymentsTable);
+                    }
                     break;
 
                 case "Quarterly":
                     logger.info("Quarterly payments selected");
-                    double[] quarterly_output = calculate.compound_interest(loan_amount_text,interest_text,years_text,4.0);
+                    paymentsAnnual.getItems().clear();
+                    double[][] quarterly_output = calculate.compound_interest(loan_amount_text, interest_text, years_text, 4.0);
                     final_text.setText("For the loan amount of " + loan_amount_text + ", at time interval of " + years_text +
-                            " years and " + weeks_text + " weeks at an interest rate of " + interest_text + " % the interest you would have to pay is $" + quarterly_output[1] +
-                            ". The total amount you wold have to pay would be $" + quarterly_output[0]);
+                            " years and " + weeks_text + " weeks at an interest rate of " + interest_text + " % the interest you would have to pay is $" + quarterly_output[0][quarterly_output[0].length - 1] +
+                            ". The total amount you wold have to pay would be $" + quarterly_output[0][quarterly_output[0].length - 1]);
                     summary.getChildren().add(final_text);
+
+                    for (int i = 1; i < quarterly_output[0].length; i++) {
+                        PaymentsTable paymentsTable = new PaymentsTable();
+                        paymentsTable.year.setValue(quarterly_output[0][i]);
+                        paymentsTable.principal.setValue(quarterly_output[1][i]);
+                        data.add(paymentsTable);
+                    }
+
                     break;
 
                 case "Weekly":
                     logger.info("Weekly payments selected");
-                    double[] weekly_output = calculate.compound_interest(loan_amount_text,interest_text,years_text,52.0);
+                    paymentsAnnual.getItems().clear();
+                    double[][] weekly_output = calculate.compound_interest(loan_amount_text, interest_text, years_text, 52.0);
                     Text weekly_text = new Text();
                     weekly_text.setText("For the loan amount of " + loan_amount_text + ", at time interval of " + years_text +
-                            " years and " + weeks_text + " weeks at an interest rate of " + interest_text + " % the interest you would have to pay is $" + weekly_output[1] +
-                            ". The total amount you wold have to pay would be $" + weekly_output[0]);
+                            " years and " + weeks_text + " weeks at an interest rate of " + interest_text + " % the interest you would have to pay is $" + weekly_output[0][weekly_output[0].length - 1] +
+                            ". The total amount you wold have to pay would be $" + weekly_output[0][weekly_output[0].length - 1]);
                     summary.getChildren().add(weekly_text);
+
+                    for (int i = 1; i < weekly_output[0].length; i++) {
+                        PaymentsTable paymentsTable = new PaymentsTable();
+                        paymentsTable.year.setValue(weekly_output[0][i]);
+                        paymentsTable.principal.setValue(weekly_output[1][i]);
+                        data.add(paymentsTable);
+                    }
                     break;
 
                 case "Fortnightly":
                     logger.info("Fortnightly payments selected");
-                    double[] fortnightly_output = calculate.compound_interest(loan_amount_text,interest_text,years_text,26.0);
+                    paymentsAnnual.getItems().clear();
+                    double[][] fortnightly_output = calculate.compound_interest(loan_amount_text, interest_text, years_text, 26.0);
                     final_text.setText("For the loan amount of " + loan_amount_text + ", at time interval of " + years_text +
-                            " years and " + weeks_text + " at an interest rate of " + interest_text + " % the interest you would have to pay is $" + fortnightly_output[1] +
-                            ". The total amount you wold have to pay would be $" + fortnightly_output[0]);
+                            " years and " + weeks_text + " at an interest rate of " + interest_text + " % the interest you would have to pay is $" + fortnightly_output[0][fortnightly_output[0].length - 1] +
+                            ". The total amount you wold have to pay would be $" + fortnightly_output[0][fortnightly_output[0].length - 1]);
                     summary.getChildren().add(final_text);
+
+                    for (int i = 1; i < fortnightly_output[0].length; i++) {
+                        PaymentsTable paymentsTable = new PaymentsTable();
+                        paymentsTable.year.setValue(fortnightly_output[0][i]);
+                        paymentsTable.principal.setValue(fortnightly_output[1][i]);
+                        data.add(paymentsTable);
+                    }
+
                     break;
 
                 case "Bi-Monthly":
                     logger.info("Bi-Monthly payments selected");
-                    double[] bimonthly_output = calculate.compound_interest(loan_amount_text,interest_text,years_text,6.0);
+                    paymentsAnnual.getItems().clear();
+                    double[][] bimonthly_output = calculate.compound_interest(loan_amount_text, interest_text, years_text, 6.0);
                     final_text.setText("For the loan amount of " + loan_amount_text + ", at time interval of " + years_text +
-                            " years and " + weeks_text + " at an interest rate of " + interest_text + " % the interest you would have to pay is $" + bimonthly_output[1] +
-                            ". The total amount you wold have to pay would be $" + bimonthly_output[0]);
+                            " years and " + weeks_text + " at an interest rate of " + interest_text + " % the interest you would have to pay is $" + bimonthly_output[0][bimonthly_output[0].length - 1] +
+                            ". The total amount you wold have to pay would be $" + bimonthly_output[0][bimonthly_output[0].length - 1]);
                     summary.getChildren().add(final_text);
+
+                    for (int i = 1; i < bimonthly_output[0].length; i++) {
+                        PaymentsTable paymentsTable = new PaymentsTable();
+                        paymentsTable.year.setValue(bimonthly_output[0][i]);
+                        paymentsTable.principal.setValue(bimonthly_output[1][i]);
+                        data.add(paymentsTable);
+                    }
                     break;
 
                 case "Daily":
                     logger.info("Bi-Monthly payments selected");
-                    double[] daily_output = calculate.compound_interest(loan_amount_text,interest_text,years_text,365.0);
+                    paymentsAnnual.getItems().clear();
+                    double[][] daily_output = calculate.compound_interest(loan_amount_text, interest_text, years_text, 365.0);
                     final_text.setText("For the loan amount of " + loan_amount_text + ", at time interval of " + years_text +
-                            " years and " + weeks_text + " at an interest rate of " + interest_text + " % the interest you would have to pay is $" + daily_output[1] +
-                            ". The total amount you wold have to pay would be $" + daily_output[0]);
+                            " years and " + weeks_text + " at an interest rate of " + interest_text + " % the interest you would have to pay is $" + daily_output[0][daily_output[0].length - 1] +
+                            ". The total amount you wold have to pay would be $" + daily_output[0][daily_output[0].length - 1]);
                     summary.getChildren().add(final_text);
+                    for (int i = 1; i < daily_output[0].length; i++) {
+                        PaymentsTable paymentsTable = new PaymentsTable();
+                        paymentsTable.year.setValue(daily_output[0][i]);
+                        paymentsTable.principal.setValue(daily_output[1][i]);
+                        data.add(paymentsTable);
+                    }
                     break;
             }
         });
