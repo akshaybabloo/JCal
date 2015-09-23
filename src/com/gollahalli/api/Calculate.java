0803640073bb23b834e79cae66c049a56da4db49
Akshay Raj Gollahalli <akshaybabloo@gmail.com>
2015-09-23 16:18:24 +1200

    javadocs added

--- src/com/gollahalli/api/Calculate.java
+++ src/com/gollahalli/api/Calculate.java
@@ -7,7 +7,7 @@ import java.math.BigDecimal;
 import java.math.RoundingMode;
 
 /**
- * Created by akshayrajgollahalli on 15/09/15.
+ * This class return all the calculations for mortgages.
  */
 public class Calculate {
 
@@ -29,6 +29,29 @@ public class Calculate {
         return total;
     }
 
+    /**
+     * This function returns the return on investment compounded over years.
+     *
+     * To use this code do the following:
+     *
+     * <pre>
+     *     {@code
+     *       Calculate calculate = new Calculate();
+     *       double[][] yearly_output = calculate.compoundInterest(10000, 5, 5, 12);
+     *
+     *       for (int i = 1; i < yearly_output[0].length; i++) {
+     *           System.out.println(yearly_output[0][i]); // number of years
+     *           System.out.println(yearly_output[1][i]); // principal amount over the years
+     *       }
+     *     }
+     * </pre>
+     *
+     * @param principal  loan amount.
+     * @param rate       Annual rate of interest.
+     * @param duration   Duration in years.
+     * @param compounded Compounded for.
+     * @return double[][] A double array is returned.
+     */
     public double[][] compoundInterest(double principal, double rate, double duration, double compounded) {
         logger.info("compound interest called with principal = " + principal + ", rate = " + rate + ", duration = " + duration +
                 " and compounded for " + compounded);
@@ -48,16 +71,29 @@ public class Calculate {
         return total;
     }
 
+    /**
+     * This function gives you the monthly mortgage payments.
+     *
+     * To use this function do the following:
+     *
+     * <pre>
+     *     {@code
+     *     Calculate calculate = New Calculate();
+     *     double monthly_output = calculate.fixedRateMortgageMonthly(10000, 5*12, 5);
+     *     System.out.println(monthly_output);
+     *     }
+     * </pre>
+     *
+     * @param loanAmount        Loan amount required.
+     * @param termInMonths      Term in months.
+     * @param interestRate      Rate of interest per annam.
+     * @return double           Returns a double number.
+     */
     public double fixedRateMortgageMonthly(double loanAmount, double termInMonths, double interestRate){
-
         BigDecimal bd;
-
         double initalInterestRate = interestRate/100;
-
         double monthlyRate = initalInterestRate / 12.0;
-
         bd = new BigDecimal((loanAmount*monthlyRate) / (1-Math.pow(1+monthlyRate, -termInMonths))).setScale(2, RoundingMode.HALF_DOWN);
-
         return bd.doubleValue();
     }
 
@@ -77,6 +113,37 @@ public class Calculate {
 //        return monthlyPayment;
 //    }
 
+    /**
+     * This function returns interest, principal and balance.
+     *
+     * To use this function, do the following:
+     *
+     * <pre>
+     *     {@code
+     *       Calculate calculate = new Calculate();
+     *
+     *       double[][] monthly_chart = calculate.fixedRateMortgageMonthlyChart(loan_amount_text, interest_text, years_text_month + months_text);
+     *       System.out.println(monthly_chart[2][0]); // principal over a month
+     *       System.out.println(monthly_chart[1][0]); // interest over a month
+     *       System.out.println(monthly_chart[3][0]); // balance
+     *       System.out.println(monthly_chart[4][0]); // Annual interest converted to one month, this will not change
+     *
+     *       for (int i = 1; i < years_text_month; i++) {
+     *           PaymentsTable paymentsTable = new PaymentsTable();
+     *           monthly_chart = calculate.fixedRateMortgageMonthlyChart(monthly_chart[3][0], interest_text, years_text_month - i);
+     *           System.out.println(monthly_chart[2][0]);
+     *           System.out.println(monthly_chart[1][0]);
+     *           System.out.println(monthly_chart[3][0]);
+     *       }
+     *     }
+     * </pre>
+     *
+     *
+     * @param principal         Loan amount.
+     * @param interest          Annual rate of interest.
+     * @param term              Term in months
+     * @return double[][]       Returns a double array
+     */
     public double[][] fixedRateMortgageMonthlyChart(double principal, double interest, double term){
 
         double[][] total = new double[5][30];
@@ -88,7 +155,7 @@ public class Calculate {
         BigDecimal bd4;
         for (int i = 0; i < 1; i++) {
             double interest1 = interest/100.0;
-            bd = new BigDecimal(interest1 / 12.0).setScale(2, RoundingMode.HALF_DOWN);
+            bd = new BigDecimal(interest1 / 12.0).setScale(5, RoundingMode.HALF_DOWN);
             total[4][i] = bd.doubleValue();
 
             bd1 = new BigDecimal(interest/100).setScale(2, RoundingMode.HALF_DOWN);
