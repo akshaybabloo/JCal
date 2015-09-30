d8e5d2cbe3a351d601cbd17f454975712ff1ae5b
Akshay Raj Gollahalli <akshaybabloo@gmail.com>
2015-10-01 12:40:20 +1300

    fresh update

---
+++ src/com/gollahalli/api/Calculate.java
@@ -0,0 +1,198 @@
+/*
+ * Copyright (c) 2015 Akshay Raj Gollahalli
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License along
+ *  with this program; if not, write to the Free Software Foundation, Inc.,
+ *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+package com.gollahalli.api;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+/**
+ * This class return all the calculations for mortgages.
+ */
+public class Calculate {
+
+    public static final Logger logger = LoggerFactory.getLogger(Calculate.class);
+
+    public double percentage(double rate) {
+        return rate / 100;
+    }
+
+    /**
+     * This function calculates simple interest as return on investment.
+     *
+     * To use this function, do the following:
+     *
+     * <pre>
+     *     {@code
+     *     Calculate calculate = new Calculate();
+     *     double[] simple = calculate.simpleInterest(10000, 5, 5);
+     *     System.out.println(simple[0]); // Simple interest
+     *     System.out.println(simple[1]); // Interest earned
+     *     }
+     * </pre>
+     *
+     * @param principal         Loan amount.
+     * @param rate              Anual rate of interest.
+     * @param duration          Duration in years.
+     * @return double[]         An double array is returned.
+     */
+    public double[] simpleInterest(double principal, double rate, double duration) {
+        logger.info("simple interest called with principal = " + principal + ", rate = " + rate + " and duration = " + duration);
+        double[] total = new double[2];
+        if (rate == 0) {
+            logger.error("Rate cannot be 0");
+        }
+        double percentRate = percentage(rate);
+        total[0] = principal * percentRate * duration;
+        total[1] = total[0] - principal;
+        return total;
+    }
+
+    /**
+     * This function returns the return on investment compounded over years.
+     *
+     * To use this code do the following:
+     *
+     * <pre>
+     *     {@code
+     *       Calculate calculate = new Calculate();
+     *       double[][] yearlyOutput = calculate.compoundInterest(10000, 5, 5, 12);
+     *
+     *       for (int i = 1; i < yearlyOutput[0].length; i++) {
+     *           System.out.println(yearlyOutput[0][i]); // number of years
+     *           System.out.println(yearlyOutput[1][i]); // principal amount over the years
+     *       }
+     *     }
+     * </pre>
+     *
+     * @param principal  loan amount.
+     * @param rate       Annual rate of interest.
+     * @param duration   Duration in years.
+     * @param compounded Compounded for.
+     * @return double[][] A double array is returned.
+     */
+    public double[][] compoundInterest(double principal, double rate, double duration, double compounded) {
+        logger.info("compound interest called with principal = " + principal + ", rate = " + rate + ", duration = " + duration +
+                " and compounded for " + compounded);
+        int durationInt = (int) duration;
+        double[][] total = new double[2][durationInt + 1];
+        double percentRate = percentage(rate);
+        BigDecimal bd;
+        BigDecimal bd1;
+        for (int i = 0; i < duration + 1; i++) {
+            total[0][i] = principal * Math.pow(1 + (percentRate / compounded), compounded * i);
+            bd = new BigDecimal(total[0][i]).setScale(2, RoundingMode.HALF_DOWN);
+            total[0][i] = bd.doubleValue();
+            total[1][i] = total[0][i] - principal;
+            bd1 = new BigDecimal(total[1][i]).setScale(2, RoundingMode.HALF_DOWN);
+            total[1][i] = bd1.doubleValue();
+        }
+        return total;
+    }
+
+    /**
+     * This function gives you the monthly mortgage payments.
+     *
+     * To use this function do the following:
+     *
+     * <pre>
+     *     {@code
+     *     Calculate calculate = New Calculate();
+     *     double monthlyOutput = calculate.fixedRateMortgageMonthly(10000, 5*12, 5);
+     *     System.out.println(monthlyOutput);
+     *     }
+     * </pre>
+     *
+     * @param loanAmount        Loan amount required.
+     * @param termInMonths      Term in months.
+     * @param interestRate      Rate of interest per annam.
+     * @return double           Returns a double number.
+     */
+    public double fixedRateMortgageMonthly(double loanAmount, double termInMonths, double interestRate){
+        BigDecimal bd;
+        double initalInterestRate = interestRate/100;
+        double monthlyRate = initalInterestRate / 12.0;
+        bd = new BigDecimal((loanAmount*monthlyRate) / (1-Math.pow(1+monthlyRate, -termInMonths))).setScale(2, RoundingMode.HALF_DOWN);
+        return bd.doubleValue();
+    }
+
+    /**
+     * This function returns interest, principal and balance.
+     *
+     * To use this function, do the following:
+     *
+     * <pre>
+     *     {@code
+     *       Calculate calculate = new Calculate();
+     *
+     *       double[][] monthlyChart = calculate.fixedRateMortgageMonthlyChart(loanAmountText, interestText, yearsTextMonth + monthsText);
+     *       System.out.println(monthlyChart[2][0]); // principal over a month
+     *       System.out.println(monthlyChart[1][0]); // interest over a month
+     *       System.out.println(monthlyChart[3][0]); // balance
+     *       System.out.println(monthlyChart[4][0]); // Annual interest converted to one month, this will not change
+     *
+     *       for (int i = 1; i < yearsTextMonth + monthsText; i++) {
+     *           PaymentsTable paymentsTable = new PaymentsTable();
+     *           monthlyChart = calculate.fixedRateMortgageMonthlyChart(monthlyChart[3][0], interestText, yearsTextMonth + monthsText - i);
+     *           System.out.println(monthlyChart[2][0]);
+     *           System.out.println(monthlyChart[1][0]);
+     *           System.out.println(monthlyChart[3][0]);
+     *       }
+     *     }
+     * </pre>
+     *
+     *
+     * @param principal         Loan amount.
+     * @param interest          Annual rate of interest.
+     * @param term              Term in months
+     * @return double[][]       Returns a double array
+     */
+    public double[][] fixedRateMortgageMonthlyChart(double principal, double interest, double term){
+
+        double[][] total = new double[5][30];
+        Calculate calculate = new Calculate();
+        BigDecimal bd;
+        BigDecimal bd1;
+        BigDecimal bd2;
+        BigDecimal bd3;
+        BigDecimal bd4;
+        for (int i = 0; i < 1; i++) {
+            double interest1 = interest/100.0;
+            bd = new BigDecimal(interest1 / 12.0).setScale(5, RoundingMode.HALF_DOWN);
+            total[4][i] = bd.doubleValue();
+
+            bd1 = new BigDecimal(interest/100).setScale(2, RoundingMode.HALF_DOWN);
+            total[0][i] = bd1.doubleValue();
+
+            bd2 = new BigDecimal((total[0][i]/12)*principal).setScale(2, RoundingMode.HALF_DOWN);
+            total[1][i]= bd2.doubleValue();
+
+            bd3 = new BigDecimal(calculate.fixedRateMortgageMonthly(principal, term, interest) - total[1][i]).setScale(2, RoundingMode.HALF_DOWN);
+            total[2][i] = bd3.doubleValue();
+
+            bd4 = new BigDecimal(principal - total[2][i]).setScale(2, RoundingMode.HALF_DOWN);
+            total[3][i] = bd4.doubleValue();
+        }
+
+        return total;
+    }
+
+}
---
+++ src/com/gollahalli/main/App.java
@@ -0,0 +1,95 @@
+/*
+ * Copyright (c) 2015 Akshay Raj Gollahalli
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License along
+ *  with this program; if not, write to the Free Software Foundation, Inc.,
+ *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+package com.gollahalli.main;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+
+/**
+ * This class is the main class which starts the software.
+ *
+ * @author Akshay Raj Gollahalli
+ */
+public class App extends Application {
+
+    public static final Logger logger = LoggerFactory.getLogger(App.class);
+    public static double JAVA_VERSION = getVersion();
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    /**
+     * This function returns a function number eg: 1.8
+     *
+     * @return double version number
+     */
+    static double getVersion() {
+        String version = System.getProperty("java.version");
+        int pos = version.indexOf('.');
+        pos = version.indexOf('.', pos + 1);
+        return Double.parseDouble(version.substring(0, pos));
+    }
+
+    /**
+     * This function first check the Java version to be equal to 1.8 or more, if not an error is give.
+     * If no error is found, FXML file is loaded.
+     *
+     * @param primaryStage Primary stage.
+     */
+    @Override
+    public void start(Stage primaryStage) {
+        if (JAVA_VERSION < 1.8) {
+            logger.error("Java version error. version " + JAVA_VERSION + " installed");
+
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Error Dialog");
+            alert.setHeaderText("There seems to be an error!");
+            alert.setContentText("You are using Java version " + JAVA_VERSION + " to run this application you " +
+                    "you need to download the latest version of Java. please go to http://www.java.com");
+            alert.showAndWait();
+        }
+        Parent root = null;
+        try {
+            root = FXMLLoader.load(getClass().getResource("/resource/JCal-gui.fxml"));
+            logger.info("JCal-gui loaded successfully");
+        } catch (IOException e) {
+            e.printStackTrace();
+            logger.error("couldn't load JCal-gui. Ended with exception " + e);
+        }
+
+        Scene scene = new Scene(root, 800, 700);
+        primaryStage.getIcons().add(new Image("/resource/JCal-logo.png"));
+        primaryStage.setResizable(false);
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("JCal - Loan calculator");
+        primaryStage.show();
+
+
+    }
+}
